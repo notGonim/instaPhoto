deb751720d79a6f443de8db067e8ff7f19262f4e
Mahmoud Ghonamie <Mahmoud.Ghonamie@orascom.com>
2023-01-11 13:48:21 +0200

    init: ADDED MAIN STRUCTURE FOR RESPONSIVE

---
+++ lib/responsive/responsive_screen_layout.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:instaphoto/utils/global_variables.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScreenLayout;
+  final Widget webScreenLayout;
+
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > webScreenSize) {
+        return webScreenLayout;
+      }
+      return mobileScreenLayout;
+    });
+  }
+}
